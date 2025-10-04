8e864481f617889d0bb8ac4f5d75ba3fab60b5dc
Blaze <manikanta3.mk1@gmail.com>
2025-10-04 23:47:49 +0530

    modified directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import NavBar from "./components/NavBar";
-import Hero from "./components/hero/hero";
+import Hero from "./components/hero/Hero.jsx";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
